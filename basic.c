a6380191a32ae086f79a2e17d1315a2a3ca526e0
Alvin Zhu <alvinzhu33@yahoo.com>
2016-10-14 15:24:05 -0400

    Started begging stuff

---
+++ basic.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "basic.h"
+
+song_node *table[26];
+
+struct song_node * insert_front(struct song_node *song){
+  return 0;
+};
+struct song_node * insert_order(struct song_node *song){
+  return 0;
+};
+struct song_node * print_struct(){
+  return 0;
+};
+struct song_node * find_name(struct song_node *song){
+  return 0;
+};
+struct song_node * find_artist(struct song_node *arist){
+  return 0;
+};
+struct song_node * find_random(){
+  return 0;
+};
+struct song_node * remove_song(){
+  return 0;
+};
+struct song_node * freeL(){
+  return 0;
+};
+
+int main(){
+  return 0;
+}
--- basic.h
+++ basic.h
@@ -4,11 +4,11 @@ typedef struct song_node{
   struct song_node *next;
 } song_node;
 
-struct song_node * insert_front();
-struct song_node * insert_order();
+struct song_node * insert_front(struct song_node *song);
+struct song_node * insert_order(struct song_node *song);
 struct song_node * print_struct();
-struct song_node * find_name();
-struct song_node * find_artist();
+struct song_node * find_name(struct song_node *song);
+struct song_node * find_artist(struct song_node *arist);
 struct song_node * find_random();
-struct song_node * remove();
+struct song_node * remove_song();
 struct song_node * freeL();
