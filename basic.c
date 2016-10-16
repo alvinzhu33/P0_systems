0fb839d5bcd9595fbbcb1c1796a76c46670941dc
Alvin Zhu <alvinzhu33@yahoo.com>
2016-10-16 00:14:50 -0400

    Remove + free

--- basic.c
+++ basic.c
@@ -48,42 +48,92 @@ void print_struct(song_node *list){
     return 0;*/
     printf("%s-%s ", list->name, list->artist);
     list = list->next;
-    while(list){
+    while(list->name){
         printf("-> %s-%s ", list->name, list->artist);
         list = list->next;
     }
     printf("\n");
 };
-song_node * find_name(song_node *song){
+song_node * find_name(song_node *list, char name[256]){
+    while(list){
+        if(strcmp(list->name, name)==0){
+            return list;
+        }
+        list->next;
+    }
     return 0;
 };
-song_node * find_artist(song_node *arist){
+song_node * find_artist(song_node *list, char artist[256]){
+    while(list){
+        if(strcmp(list->artist, artist)==0){
+            return list;
+        }
+        list->next;
+    }
     return 0;
 };
-song_node * find_random(){
-    return 0;
+song_node * find_random(song_node *list){
+    int counter=0;
+    song_node *index;
+    while(index){
+        counter++;
+        index->next;
+    }
+    int ran = rand() % counter;
+    while(counter){
+        list->next;
+        counter--;
+    }
+    return list;
 };
-song_node * remove_song(){
-    return 0;
+song_node * remove_song(song_node *list, char name[256]){
+    song_node *current = list;
+    if(strcmp(name, current->name)==0){
+        return list->next;
+    }
+    while(current->next){
+        if(strcmp(name, current->next->name)==0){
+            current->next = current->next->next;
+            return list;
+        }
+        current = current->next;
+    }
+    return list;
 };
-song_node * freeL(){
-    return 0;
+song_node * freeL(song_node *list){
+    song_node *current = list;
+    song_node *nextSong = (song_node *)malloc(sizeof(song_node));
+    while(current){
+        nextSong = current->next;
+        free(current);
+        current = nextSong;
+    }
+    return list;
 };
 
 int main(){
     song_node *try;
     try = (song_node *)malloc(sizeof(song_node));
-    strcpy(try->name, "boom boom pow");
+    strcpy(try->name,"boom boom pow");
     strcpy(try->artist, "black eyed peas");
     print_struct(try);
-    //try = insert_front(try, "red", "tSwift");
-    //try = insert_front(try, "diamon", "rihanna");
+    printf("\nTesting insert\n");
     try = insert_order(try, "true colors", "cyndi lauper");
     try = insert_order(try, "hello", "adele");
     try = insert_order(try, "lights", "ellie goulding");
     try = insert_order(try, "get lucky", "daft punk");
     print_struct(try);
-    //int i = strcmp("add", "ad");
+    printf("\nTesting delete\n");
+    try = remove_song(try, "get lucky");
+    try = remove_song(try, "lights");
+    try = remove_song(try, "hello");
+    print_struct(try);
+    freeL(try);
+    //print_struct(try);
+    /*printf("%s\n", find_artist(try, "daft punk"));
+    printf("%s\n", find_artist(try, "adele"));
+    printf("%s\n", find_artist(try, "ellie goulding"));
+    printf("%s\n", find_artist(try, "fetty wap"));*/
 
     /*printf("%d\n", i);
     //printf("%d\n", artist_where(try));
--- basic.h
+++ basic.h
@@ -7,8 +7,8 @@ typedef struct song_node{
 song_node * insert_front(song_node *list, char name[256], char artist[256]);
 song_node * insert_order(song_node *list, char name[256], char artist[256]);
 void print_struct(song_node *list);
-song_node * find_name(song_node *song);
-song_node * find_artist(song_node *arist);
-song_node * find_random();
-song_node * remove_song();
-song_node * freeL();
+song_node * find_name(song_node *list, char name[256]);
+song_node * find_artist(song_node *list, char artist[256]);
+song_node * find_random(song_node *list);
+song_node * remove_song(song_node *list, char name[256]);
+song_node * freeL(song_node *list);
